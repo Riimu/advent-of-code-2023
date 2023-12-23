ec6b17b375d0ea4a4fdc85e87e27270c0c368ced
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2023-12-23 19:57:38 +0200

    Use same solution for both Day 23 parts

--- src/Task/Day23/AbstractDay23Task.php
+++ src/Task/Day23/AbstractDay23Task.php
@@ -6,6 +6,7 @@ namespace Riimu\AdventOfCode2023\Task\Day23;
 
 use Riimu\AdventOfCode2023\TaskInputInterface;
 use Riimu\AdventOfCode2023\TaskInterface;
+use Riimu\AdventOfCode2023\Utility\Direction;
 use Riimu\AdventOfCode2023\Utility\Parse;
 
 /**
@@ -36,5 +37,138 @@ abstract class AbstractDay23Task implements TaskInterface
         return (string) $this->solve($input);
     }
 
-    abstract protected function solve(Day23Input $input): int;
+    protected function solve(Day23Input $input): int
+    {
+        $startX = 0;
+        $startY = 0;
+        $endX = 0;
+        $endY = \count($input->map) - 1;
+
+        foreach ($input->map[$startY] as $x => $node) {
+            if ($node === Day23Input::NODE_SPACE) {
+                $startX = $x;
+                break;
+            }
+        }
+
+        foreach ($input->map[$endY] as $x => $node) {
+            if ($node === Day23Input::NODE_SPACE) {
+                $endX = $x;
+                break;
+            }
+        }
+
+        $junctions = [$startY => [$startX => []], $endY => [$endX => []]];
+        $junctionExits = [];
+
+        foreach ($this->getPossibleDirections($input->map, $startX, $startY) as [$x, $y, $direction]) {
+            $junctionExits[] = [$x, $y, $direction, $startX, $startY];
+        }
+
+        while ($junctionExits !== []) {
+            [$x, $y, $direction, $lastX, $lastY] = array_pop($junctionExits);
+
+            $reversible = true;
+            $steps = 0;
+
+            while (true) {
+                $steps++;
+
+                if (isset($junctions[$y][$x])) {
+                    break;
+                }
+
+                $possibleDirections = $this->getPossibleDirections($input->map, $x, $y);
+                $otherDirections = array_diff_key($possibleDirections, [$direction->turnAround()->value => true]);
+
+                if ($otherDirections === []) {
+                    continue 2;
+                }
+
+                if ($possibleDirections === $otherDirections) {
+                    $reversible = false;
+                }
+
+                if (\count($otherDirections) > 1) {
+                    $junctions[$y][$x] = [];
+
+                    foreach ($possibleDirections as [$newX, $newY, $newDirection]) {
+                        $junctionExits[] = [$newX, $newY, $newDirection, $x, $y];
+                    }
+
+                    break;
+                }
+
+                [$x, $y, $direction] = $otherDirections[array_key_first($otherDirections)];
+            }
+
+            $junctions[$lastY][$lastX][] = [$x, $y, $steps];
+
+            if ($reversible) {
+                $junctions[$y][$x][] = [$lastX, $lastY, $steps];
+                [$exitX, $exitY] = $direction->turnAround()->moveCoordinates($x, $y);
+
+                foreach ($junctionExits as $key => $exit) {
+                    if ($exit[0] === $exitX && $exit[1] === $exitY) {
+                        array_splice($junctionExits, $key, 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+        $maxLength = 0;
+        $traverseQueue = [[$startX, $startY, 0, []]];
+
+        while ($traverseQueue !== []) {
+            [$x, $y, $steps, $visitedJunctions] = array_pop($traverseQueue);
+
+            if ($x === $endX && $y === $endY) {
+                $maxLength = max($steps, $maxLength);
+                continue;
+            }
+
+            $visitedJunctions[$y][$x] = true;
+
+            foreach ($junctions[$y][$x] as [$nextX, $nextY, $nextSteps]) {
+                if (isset($visitedJunctions[$nextY][$nextX])) {
+                    continue;
+                }
+
+                $traverseQueue[] = [$nextX, $nextY, $steps + $nextSteps, $visitedJunctions];
+            }
+        }
+
+        return $maxLength;
+    }
+
+    /**
+     * @param array<int, array<int, string>> $map
+     * @param int $x
+     * @param int $y
+     * @return array<int, array{0: int, 1: int, 2: Direction}>
+     */
+    private function getPossibleDirections(array $map, int $x, int $y): array
+    {
+        $directions = [];
+
+        foreach (Direction::cases() as $direction) {
+            [$newX, $newY] = $direction->moveCoordinates($x, $y);
+
+            if ($this->isValidDirection($map, $newX, $newY, $direction)) {
+                $directions[$direction->value] = [$newX, $newY, $direction];
+            }
+        }
+
+        return $directions;
+    }
+
+    /**
+     * @param array<int, array<int, string>> $map
+     * @param int $x
+     * @param int $y
+     * @param Direction $direction
+     * @return bool
+     */
+    abstract protected function isValidDirection(array $map, int $x, int $y, Direction $direction): bool;
 }
--- src/Task/Day23/Day23Part1Task.php
+++ src/Task/Day23/Day23Part1Task.php
@@ -13,65 +13,19 @@ use Riimu\AdventOfCode2023\Utility\Direction;
  */
 class Day23Part1Task extends AbstractDay23Task
 {
-    protected function solve(Day23Input $input): int
+    protected function isValidDirection(array $map, int $x, int $y, Direction $direction): bool
     {
-        $startX = 0;
-        $startY = 0;
-        $endX = 0;
-        $endY = \count($input->map) - 1;
-
-        foreach ($input->map[$startY] as $x => $node) {
-            if ($node === Day23Input::NODE_SPACE) {
-                $startX = $x;
-                break;
-            }
+        if (!isset($map[$y][$x])) {
+            return false;
         }
 
-        foreach ($input->map[$endY] as $x => $node) {
-            if ($node === Day23Input::NODE_SPACE) {
-                $endX = $x;
-                break;
-            }
-        }
-
-        $nodeQueue = [[$startX, $startY, 0, [$startY => [$startX => true]]]];
-        $maxLength = 0;
-
-        while ($nodeQueue !== []) {
-            [$x, $y, $steps, $visited] = array_pop($nodeQueue);
-            $steps++;
-
-            foreach ($this->getNodeDirections($input->map[$y][$x]) as $direction) {
-                [$newX, $newY] = $direction->moveCoordinates($x, $y);
-
-                if ($newX === $endX && $newY === $endY) {
-                    $maxLength = (int) max($steps, $maxLength);
-                } elseif (isset($input->map[$newY][$newX]) &&
-                    $input->map[$newY][$newX] !== Day23Input::NODE_WALL &&
-                    !isset($visited[$newY][$newX])
-                ) {
-                    $newPath = $visited;
-                    $newPath[$newY][$newX] = true;
-                    $nodeQueue[] = [$newX, $newY, $steps, $newPath];
-                }
-            }
-        }
-
-        return $maxLength;
-    }
-
-    /**
-     * @param string $node
-     * @return array<int, Direction>
-     */
-    private function getNodeDirections(string $node): array
-    {
-        return match ($node) {
-            Day23Input::NODE_SLOPE_LEFT => [Direction::LEFT],
-            Day23Input::NODE_SLOPE_RIGHT => [Direction::RIGHT],
-            Day23Input::NODE_SLOPE_UP => [Direction::UP],
-            Day23Input::NODE_SLOPE_DOWN => [Direction::DOWN],
-            default => Direction::cases(),
+        return match ($map[$y][$x]) {
+            Day23Input::NODE_WALL => false,
+            Day23Input::NODE_SLOPE_LEFT => $direction !== Direction::RIGHT,
+            Day23Input::NODE_SLOPE_RIGHT => $direction !== Direction::LEFT,
+            Day23Input::NODE_SLOPE_UP => $direction !== Direction::DOWN,
+            Day23Input::NODE_SLOPE_DOWN => $direction !== Direction::UP,
+            default => true,
         };
     }
 }
